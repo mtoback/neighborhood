f5d98b56080a2d78b86fa4fbed3d77320f7130d0
Michael Toback <mtoback@vmware.com>
2016-05-23 17:55:51 -0700

    update to allow filter and lists to execute properly

--- js/app.js
+++ js/app.js
@@ -1,6 +1,20 @@
+/**
+  This file handles three different external APIs - Google Map, Yelp, and weather
+  The Google Geocode API is used as a way to get from location to lat/lng, which
+  is needed for the weather API..
+**/
+/** declare map as a global variable so it is visible to both the view model and markers */
+var map;
+/**
+   view model handles *most* of the interaction between the view and the model. The exception
+   is when you click on the markers, either the markers themselves or the list. That is handled
+   via standard jQuery calls
+
+   The view model tracks the markers returned as a static list and the displayMarkers, markers displayed,
+   as a dynamic list.
+**/
 var viewModel = {
-	address : ko.observable('12221 Saraglen Drive'),
-	city    : ko.observable('Saratoga CA'),
+	location : ko.observable('18764 Cox Ave, Saratoga CA'),
 	searches : ko.observableArray(['Post Office', 'Restaurant', 'Grocery Stores']),
 	markers : ko.observableArray(),
 	displayMarkers: ko.observableArray(),
@@ -8,7 +22,11 @@ var viewModel = {
 	localWeather : ko.observable(),
 	filter: ko.observable(""),
 	zoom: ko.observable(12),
+	/**
+	 execute the filter. Also, clear the detail entry
+	**/
 	filterMap: function(){
+		$("#about").html('');
 		var filter = viewModel.filter();
 		if (viewModel.markers().length > 0){
 			setMapOnAll(null, viewModel.displayMarkers());
@@ -44,6 +62,7 @@ var viewModel = {
 						found_state = true;
 					}
 				}
+				// Call the weather API given the city/state associated with the lat/lng
 				$.ajax({
 					url: 'http://api.wunderground.com/api/421920ddc8bd7347/forecast/q/' + state + '/' + city + '.json',
 					success: function(result){
@@ -56,127 +75,155 @@ var viewModel = {
 			}
 		});
 	},
-	generateMap : function(){
-		$.ajax({url: 'https://maps.googleapis.com/maps/api/geocode/json?address=' + this.location(),
-			error: function (xhr, ajaxOptions, thrownError) {
-			        if (xhr.status === 404){
-			        	alert("internet failure occurred. Please check your connection");
-			        } else {
-			        	alert("unknown error occurred... status = " + xhr.status);
-			        }
-			      },
-			success: function(result){
-			  var lat = result.results[0].geometry.location.lat;
-			  var lng =  result.results[0].geometry.location.lng;
-			  viewModel.weather(lat,lng);
-			  var mapProp = {
-			    center:new google.maps.LatLng(lat,lng),
-			    zoom: viewModel.zoom(),
-			    mapTypeId:google.maps.MapTypeId.ROADMAP
-			  };
-			  $("#googleMap").show();
-			  // this should be visible at the top level to enable us to control it
-			  map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
-			  var auth = {
-			    consumerKey : "IuMIJl585jfMDHz7spOj3w",
-			    consumerSecret : "8qnDc0yEa59ZHfXN1uM3XY-RcW4",
-			    accessToken : "bfEYLApv2Hxfs0vDEh6TMRsOllsGYEG1",
-			    accessTokenSecret : "zcavaTCQX2wlPIjvm4DqUzzGV94",
-			    serviceProvider : {
-			        signatureMethod : "HMAC-SHA1"
-			    }
-			  };
 
-			  var terms = 'food';
-			  var near = viewModel.location();
-
-				var accessor = {
-				    consumerSecret : auth.consumerSecret,
-				    tokenSecret : auth.accessTokenSecret
-				};
-				var parameters = {
-				      oauth_consumer_key: "IuMIJl585jfMDHz7spOj3w",
-				      oauth_token: "bfEYLApv2Hxfs0vDEh6TMRsOllsGYEG1",
-				      oauth_nonce: nonce_generate(),
-				      oauth_timestamp: Math.floor(Date.now()/1000),
-				      oauth_signature_method: 'HMAC-SHA1',
-				      oauth_version : '1.0',
-				      callback: 'cb',
-				      term: "restaurant",
-				      location: viewModel.location(),
-				      radius_filter: viewModel.radius_filter()
-				    };
-				var message = {
-				    'action' : 'http://api.yelp.com/v2/search',
-				    'method' : 'GET',
-				    'parameters' : parameters
-				};
-
-				function nonce_generate() {
-				  return (Math.floor(Math.random() * 1e12).toString());
-				}
-				var yelp_url = "https://api.yelp.com/v2/search";
-				var encodedSignature = oauthSignature.generate('GET',yelp_url, parameters, "8qnDc0yEa59ZHfXN1uM3XY-RcW4",
-					"zcavaTCQX2wlPIjvm4DqUzzGV94");
-			    parameters.oauth_signature = encodedSignature;
-                $.ajax({
-			      url: yelp_url,
-			      data: parameters,
-			      cache: true,                // This is crucial to include as well to prevent jQuery from adding on a cache-buster parameter "_=23489489749837", invalidating our oauth-signature
-			      dataType: 'jsonp',
-			      success: function(results) {
-			        var businesses = results.businesses;
-			        businesses.forEach(function(business){
-			        	var name = business.name;
-			        	var url = business.mobile_url;
-			        	var lat = business.location.coordinate.latitude;
-			        	var lng = business.location.coordinate.longitude;
-						var myLatlng = new google.maps.LatLng(lat, lng);
-
-						var marker = new google.maps.Marker({
-						    position: myLatlng,
-							animation: google.maps.Animation.DROP,
-						    title: name,
-						    url: url
-						});
-						viewModel.markers.push(marker);
-						viewModel.displayMarkers.push(marker);
-						function toggleBounce(item) {
-						  if (marker.getAnimation() !== null) {
-						    marker.setAnimation(null);
-						  } else {
-						    marker.setAnimation(google.maps.Animation.BOUNCE);
-						  }
-						  $("#about").html("<a href='" + marker.url + "'>'" + marker.title + "</a>");
-						  $("#about").show();
-						}
-						marker.addListener('click', toggleBounce);
-						// To add the marker to the map, call setMap();
-						marker.setMap(map);
-					});
-					if (businesses.length > 0){
-						$("#search").show();
-					}
-			      },
-					error: function (xhr, ajaxOptions, thrownError) {
-					        if (xhr.status === 404){
-					        	alert("internet failure occurred. Please check your connection");
-					        } else {
-					        	alert("unknown error occurred... status = " + xhr.status);
-					        }
-					      }
-    });
-
-	}});
-},
 };
 
+/**
+   iife to get the map generated
+**/
+(function(viewModel){
+	$.ajax({url: 'https://maps.googleapis.com/maps/api/geocode/json?address=' + viewModel.location(),
+		success: function(result){
+		  lat = result.results[0].geometry.location.lat;
+		  lng =  result.results[0].geometry.location.lng;
+		  viewModel.weather(lat,lng);
+		  var mapProp = {
+		    center:new google.maps.LatLng(lat,lng),
+		    zoom: viewModel.zoom(),
+		    mapTypeId:google.maps.MapTypeId.ROADMAP
+		  };
+		  map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+		  initYelp(viewModel, map);
+		},
+		error: function (xhr, ajaxOptions, thrownError) {
+		        if (xhr.status === 404){
+		        	alert("internet failure occurred. Please check your connection");
+		        } else {
+		        	alert("unknown error occurred... status = " + xhr.status);
+		        }
+		 }
+		});
+	})(viewModel);
+	/**
+	initialize yelp and get generate marker array
+	**/
+	function initYelp(viewModel, map){
+	  var auth = {
+	    consumerKey : "IuMIJl585jfMDHz7spOj3w",
+	    consumerSecret : "8qnDc0yEa59ZHfXN1uM3XY-RcW4",
+	    accessToken : "bfEYLApv2Hxfs0vDEh6TMRsOllsGYEG1",
+	    accessTokenSecret : "zcavaTCQX2wlPIjvm4DqUzzGV94",
+	    serviceProvider : {
+	        signatureMethod : "HMAC-SHA1"
+	    }
+	  };
+
+
+		var accessor = {
+		    consumerSecret : auth.consumerSecret,
+		    tokenSecret : auth.accessTokenSecret
+		};
+		var parameters = {
+		      oauth_consumer_key: auth.consumerKey,
+		      oauth_token: auth.accessToken,
+		      oauth_nonce: nonce_generate(),
+		      oauth_timestamp: Math.floor(Date.now()/1000),
+		      oauth_signature_method: 'HMAC-SHA1',
+		      oauth_version : '1.0',
+		      callback: 'cb',
+		      term: "restaurant",
+		      location: 'Saratoga CA',
+		      radius_filter: viewModel.radius_filter()
+		};
+		var message = {
+		    'action' : 'http://api.yelp.com/v2/search',
+		    'method' : 'GET',
+		    'parameters' : parameters
+		};
+
+		var yelp_url = "https://api.yelp.com/v2/search";
+		var encodedSignature = oauthSignature.generate('GET',yelp_url, parameters, "8qnDc0yEa59ZHfXN1uM3XY-RcW4",
+			"zcavaTCQX2wlPIjvm4DqUzzGV94");
+	    parameters.oauth_signature = encodedSignature;
+        $.ajax({
+	      url: yelp_url,
+	      data: parameters,
+	      cache: true,                // This is crucial to include as well to prevent jQuery from adding on a cache-buster parameter "_=23489489749837", invalidating our oauth-signature
+	      dataType: 'jsonp',
+	      success: function(results) {
+	      	/**
+	      	for each business, get information and put into marker data
+	      	including categories for later display
+	      	**/
+	        var businesses = results.businesses;
+	        businesses.forEach(function(business){
+	        	var name = business.name;
+	        	var url = business.mobile_url;
+	        	var categories = [];
+	        	business.categories.forEach(function(category){
+	        		categories.push(category[0]);
+	        	});
+	        	var lat = business.location.coordinate.latitude;
+	        	var lng = business.location.coordinate.longitude;
+				var myLatlng = new google.maps.LatLng(lat, lng);
+
+				var marker = new google.maps.Marker({
+				    position: myLatlng,
+					animation: google.maps.Animation.DROP,
+				    title: name,
+				    url: url,
+				    categories: categories.join(", ")
+				});
+				viewModel.markers.push(marker);
+				viewModel.displayMarkers.push(marker);
+				/**
+				   when you click on a marker, you want it to bounce the marker and generate the
+				   detailed entry
+				**/
+				function toggleBounce(item) {
+				  if (marker.getAnimation() !== null) {
+				    marker.setAnimation(null);
+				  } else {
+				    marker.setAnimation(google.maps.Animation.BOUNCE);
+				  }
+				  $("#about").html("<a href='" + marker.url + "'>" + marker.title + "</a>" +
+				  	"  <span>" + marker.categories + "</span>");
+				  $("#about").show();
+				}
+				marker.addListener('click', toggleBounce);
+				// To add the marker to the map, call setMap();
+				marker.setMap(map);
+			});
+			if (businesses.length > 0){
+				$("#search").show();
+			}
+	      },
+		error: function (xhr, ajaxOptions, thrownError) {
+		        if (xhr.status === 404){
+		        	alert("internet failure occurred. Please check your connection");
+		        } else {
+		        	alert("unknown error occurred... status = " + xhr.status);
+		        }
+		      }
+		});
+	}
+	/**
+	  generate nonce for yelp api
+	**/
+	function nonce_generate() {
+	  return (Math.floor(Math.random() * 1e12).toString());
+	}
 // Sets the map on all markers in the array.
+// if map == None it clears markers from map
 function setMapOnAll(map, markers) {
   for (var i = 0; i < markers.length; i++) {
     markers[i].setMap(map);
   }
 }
+
+/**
+convert Google map zoom setting to yelp radius filter
+**/
 viewModel.radius_filter = ko.computed(function(){
 		if(this.zoom() === 11){
 			return 40000;
@@ -188,30 +235,26 @@ viewModel.radius_filter = ko.computed(function(){
 			return 10000;
 		}
 	}, viewModel);
-viewModel.location = ko.computed(function(){
-	return this.address() + ',' + this.city();
-}, viewModel);
+
+/**
+   when you click on the list, you want it to bounce the marker and generate the
+   detailed entry
+**/
+$("#search").on("click","li span", function(){
+	var title = $(this).html();
+	viewModel.displayMarkers().forEach(function(marker){
+		if (marker.title === title){
+		  if (marker.getAnimation() !== null) {
+		    marker.setAnimation(null);
+		  } else {
+		    marker.setAnimation(google.maps.Animation.BOUNCE);
+		  }
+		  $("#about").html("<a href='" + marker.url + "'>" + marker.title + "</a>" +
+						  	"  <span>" + marker.categories + "</span>");
+		  $("#about").show();
+		}
+	});
+
+});
 
 ko.applyBindings(viewModel);
-$( "#slider" ).slider({
-	  // allow value to go from 11->12->13
-      value: 250,
-      min: 0,
-      max: 500,
-      step: 250,
-      slide: function( event, ui ) {
-        var value = ui.value;
-        viewModel.zoom(11+value/250);
-      }
-    });
-function initialize() {
-  var mapProp = {
-    center:new google.maps.LatLng(51.508742,-0.120850),
-    zoom:5,
-    mapTypeId:google.maps.MapTypeId.ROADMAP
-  };
-  var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
-}
-$("#googleMap").hide();
-google.maps.event.addDomListener(window, 'load', initialize);
-viewModel.generateMap();
